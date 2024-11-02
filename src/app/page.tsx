0fd52f947521a5d351f4871f1d1ece868f52655a
ZaidAsif <zaid.asif.9082@gmail.com>
2024-11-03 01:40:41 +0500

    linting error solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function home() {
     <div className="max-w-md">
       <h1 className="mb-5 text-5xl font-bold">Welcome to <span className="text-red-500 font-bold">Blog</span><span className="text-black font-bold">it</span></h1>
       <p className="mb-5 text-m">
-"      Welcome to a Blogit where ideas flourish and stories unfold! Our blogging platform is more than just a collection of articles—it's a community of passionate voices sharing insights, experiences, and inspiration. From thought-provoking essays to lighthearted tales, there's something for everyone. Here, you can immerse yourself in a tapestry of creativity and knowledge, connecting with writers and readers who share your interests. Step in and let your curiosity lead the way; your next great adventure in reading starts here! "      </p>
+       Welcome to a Blogit where ideas flourish and stories unfold! Our blogging platform is more than just a collection of articles—it's a community of passionate voices sharing insights, experiences, and inspiration. From thought-provoking essays to lighthearted tales, there's something for everyone. Here, you can immerse yourself in a tapestry of creativity and knowledge, connecting with writers and readers who share your interests. Step in and let your curiosity lead the way; your next great adventure in reading starts here!      </p>
       <p className="font-bold text-xl">You can start even without an account</p>
       <Link href={'/home'} className="btn btn-error">Get Started</Link>
     </div>
